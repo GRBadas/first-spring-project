ed01caea79f72f1e09bfd9b4b087536f34526f36
Gabriel Badaró <grbadas@gmail.com>
2023-02-05 17:30:16 -0300

    first commit

---
+++ src/main/java/com/firstproject/userdept/FisrtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.firstproject.userdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisrtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisrtProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstproject/userdept/FisrtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstproject.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisrtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
